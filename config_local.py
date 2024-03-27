c1c622a51cd7a0984977110b92156bd03f408d65
Carl S <acasem-9@student.ltu.se>
2024-03-27 19:14:05 +0100

    config files added

---
+++ config_local.py
@@ -0,0 +1,35 @@
+import os
+
+#MODELS_DIRECTORY= os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'models'))
+# VIDEO_DIRECTORY_TEST_1 = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'data/test/test_1'))
+
+BANGLA_CONFIG_JSON = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config_bangla.json'))
+
+################################################################################################################################################################
+# Relative paths from the data directory #######################################################################################################################
+SEPARATED_C_DATA_DIR = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data/raw/separated_c_data'))
+SEPARATED_D_DATA_DIR = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data/raw/separated_d_data'))
+SEPARATED_C_DATA_DIR_SAMPLE = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data/raw/separated_c_data_sampled'))
+SEPARATED_D_DATA_DIR_SAMPLE = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data/raw/separated_d_data_sampled'))
+WORD_SEPARATED_DIR = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data/raw/word_separated'))
+BANGLA_YOLO_CLASS_MAPPING = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data/raw/word_separated/bangla_yolo_class_mapping.csv'))
+DATA_DIR = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data/'))
+
+################################################################################################################################################################
+# Relative paths from the cnet directory #######################################################################################################################
+CNET = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'cnet'))
+
+
+################################################################################################################################################################
+# Relative paths from the dnet directory #######################################################################################################################
+
+
+################################################################################################################################################################
+# Relative paths from the _ directory ##########################################################################################################################
+
+
+
+################################################################################################################################################################
+# Relative paths from the _ directory ##########################################################################################################################
+
+################################################################################################################################################################
